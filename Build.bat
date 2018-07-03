6913c1dc06a3e2b91a7bdaaf09797c3baa7c0f0f
IInspectable <github@smjh.de>
2018-07-03 20:56:50 +0200

    Build.bat

--- Build.bat
+++ Build.bat
@@ -7,4 +7,4 @@ if "%config%" == "" (
 
 "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe" "IXOS File Name Extensions.sln" /p:Configuration="%config%" /maxcpucount:3 /v:n
 
-Pause
+REM Pause
